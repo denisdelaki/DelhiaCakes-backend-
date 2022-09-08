7cec52904f1da359455e7e4c0e44d1c12457296f
denisdelaki <denisdelaki@gmail.com>
2022-09-08 12:09:18 +0300

    added attributes to the cakes table

---
+++ db/migrate/20220908083844_create_cakes.rb
@@ -0,0 +1,12 @@
+class CreateCakes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cakes do |t|
+     t.string :name
+     t.integer :price 
+     t.string :quantity
+     t.integer :likes
+     t.integer :available
+     t.string :image
+    end
+  end
+end
---
+++ db/migrate/20220908084051_create_orders.rb
@@ -0,0 +1,4 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
