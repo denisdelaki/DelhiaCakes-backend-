57f3947db7aa411056028356541d3ce4e969aead
denisdelaki <denisdelaki@gmail.com>
2022-09-09 14:05:07 +0300

    created routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,37 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/cakes" do
+    cakes=Cake.all 
+    cakes.to_json
   end
-
+  patch "/cakes/:id" do
+  like=Cake.find(params[:id])
+  like.update(
+    likes: params[:likes]
+  )
+  like.to_json
+  end
+get "/orders" do
+    orders=Order.all 
+    orders.to_json
+  end
+  post "/orders" do
+    customer=Customer.create(
+    fullname: params[:fullname],
+    email: params[:email],
+    phonenumber: params[:phonenumber]
+    )
+    customer.to_json
+  end
+  post "/orders" do
+    order=Order.create(
+    fullname: params[:fullname],
+    email: params[:email],
+    phonenumber: params[:phonenumber],
+    image: params[:image],
+    description: params[:description]
+    )
+    order.to_json
+  end
 end
